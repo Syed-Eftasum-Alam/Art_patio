983051c3a1999218c2eab719782ff935b10c2629
Syed Eftasum Alam <62606974+Syed-Eftasum-Alam@users.noreply.github.com>
2023-05-06 03:43:52 +0600

    Add files via upload

---
+++ html/aboutUs/About_us/About_us.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="style.css" />
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"
+    ></script>
+    <style type="text/css">
+  .form:hover{
+    box-shadow: 1px 2px 10px 5px #375554;
+  }
+/*//header*/
+  .menu{
+      background-color: #488280 !important;
+      height: 120px;
+   }
+   .image{
+      height: 200px;
+      margin-top: -42px;
+      margin-left: 36%;
+   }
+   
+   .hor{
+   height: 3px;
+   margin-top: 0px;
+   background-color: white;
+  }
+
+
+/*footer*/
+
+
+.social{
+   margin-left: 44.5%;
+   margin-bottom: 10px;
+   width: 200px;
+}
+.center{
+   margin-left: 2.5%;
+}
+
+.image1{
+   height: 50px;
+   width: 40px;
+   margin-right: 10px;
+}
+
+.image2{
+   height: 50px;
+   width: 40px;
+   margin-left: 10px;
+}
+.image3{
+   height: 50px;
+   width: 40px;
+}
+.fa{
+   width: 50px;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.section{
+   margin-top: 150px;
+
+}
+.ft1{
+  margin-left: 0px;
+}
+.y{
+  margin-left: -10px;
+}
+.contact{
+  display: flex;
+  font-weight: 200px;
+  font-size: 20px;
+  color:#555555;
+  margin-top: 250px;
+
+}
+.contactbtn{
+  text-decoration: none;
+}
+
+.contactbtn:hover{
+  text-shadow: 3px 3px 8px #488280;
+  text-decoration: none;
+ 
+
+}
+.faq{
+  display: flex;
+  font-weight: 200px;
+  font-size: 20px;
+  color:#555555;
+  margin-top: -220px;
+
+}
+.sp{
+display: flex;
+margin-left: 20px;
+
+}
+.sp:hover{
+  text-shadow: 3px 3px 8px #488280;
+}
+
+
+
+</style>
+  </head>
+  <body>
+
+    <nav class ="menu">
+      <a href="../../index.php"><img class="image" src="../../6.jpg"></a>
+      
+   </nav>
+  <hr class="hor">
+
+  <div class="center y">
+
+
+  <!-- <body class="center "> -->
+    <div class="container">
+      <span class="big-circle"></span>
+      <img src="img/shape.png" class="square" alt="" />
+      <div class="form"  >
+        <div class="contact-info">
+          <h3 class="title">About Us</h3>
+          <p class="text">
+            "Art Patio" is the art marketplace and print-on-demand website.We've been helping artists sell arts. This is the home to hundreds of artists, gallery and art collector.<br>
+            With just a few clicks, artists can upload their art images to ArtPatio,
+ set their prices for hundreds of different print-on-demand products,
+ and then instantly sell those products to an audience of online,
+ mobile, and real-world buyers. Art Patio fulfills each order by taking care of the printing, framing, matting, packaging, shipping, collecting payments from 
+the buyers. In addition to providing an online marketplace and fulfillment service, we plan for the future to provide artists,
+photographers and every small business owner a platform so that it is benificial for young artists and entrepreneur, with sales and marketing tools to help simplify and accelerate their careers.
+          </p>
+
+          
+          <div class = "center">
+          <div class="social-media" >
+            <div > Contact With Us Via  </div>
+            <div class="social-icons ">
+              <a href="https://www.facebook.com/messages/t/syed.sazin">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="https://twitter.com/">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="https://instagram.com/">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="mailto:salam201133@bscse.uiu.ac.bd">
+                <i class="fab fa-google"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        </div>
+ 
+          
+        </div>
+      </div>
+    </div>
+
+    <script src="app.js"></script>
+  </div>
+
+<div class="ft1">
+  <p ><h2 class="sp" style="color:#555555; margin-left: 20px;">SUPPORT</h2></p>
+  <footer class="section">
+
+
+
+
+<div class="contact">
+    <a href="../../contact us page\ContactUs\contact_us.php" class="contactbtn"style="color:#555555; margin-top: -250px; margin-left: 20px;">Contact Us</a>
+  </div>
+
+  <div class="faq">
+    <a href="../../faq us page\faq_Us\faq_us.php" class="contactbtn"style="color:#555555; margin-left: 20px;">FAQ</a>
+  
+  
+  </div>
+
+
+
+  <!-- <div class="center #375554-text x" style="color:#375554">Copyright from @2022</div> -->
+  <div class="social">
+    <a href="http://facebook.com"><img class="fa image1" src="../../image\fb1.png" ></a>
+  <a href="http://instagram.com"><img class="fa image3"  src="../../image\ins1.png" ></a>
+  <a href="http://twitter.com"><img class="fa image2" src="../../image\tw1.png" ></a>
+  </div>
+</footer>
+
+
+
+
+
+
+</div> 
+
+
+
+
+
+  </body>
+</html>
